09d2fd559d77df8aa2f617e2dd2178b27e088c7a
Subramanyam <sirisalasubramanyam@gmail.com>
2025-08-08 11:16:08 +0530

    new ADO.net project

---
+++ ADO.Net_project/ADO.Net_P1/ADO.Net_P1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.Net_P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter FullName : ");
+            string fn = Console.ReadLine();
+
+            Console.Write("enter age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            string qry= $"insert into ClinicPatient values('{fn}',{age})";
+            //string qry = $"delete  ClinicPatient where PatientID=(select max(PatientID) from ClinicPatient)";
+            //string qry = $"update  ClinicPatient set FullName='patient' where PatientID=(select max(PatientID) from ClinicPatient)";
+
+
+            //string qry= $"select * from ClinicPatient";
+
+            SqlConnection cn = new SqlConnection("data source=DESKTOP-UPK6IVO; initial catalog=subbu; integrated security=sspi");
+            SqlCommand cmd = new SqlCommand(qry, cn);
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            //cmd.ExecuteReader();
+            cn.Close();
+        }
+    }
+}
+//ClinicPatient
+//DESKTOP-UPK6IVO
+//subbu
+//(PatientID,FullName, Age)
